00d19e252041aebfc701f3a3cfad2cf2d6138804
ncsumarit <jen_riehle@ncsu.edu>
2014-01-27 16:15:20 -0500

    New template for homepage, new features and functionality extended from original theme.

--- functions.php
+++ functions.php
@@ -9,10 +9,25 @@
 
 /* ------------------------------------------------------------------------- *
  *  Load theme files
-/* ------------------------------------------------------------------------- */	
+/* ------------------------------------------------------------------------- */
 
 function alx_load() {
+// Load theme options and meta boxes
+	load_template( get_stylesheet_directory() . '/functions/theme-options.php' );
+	load_template( get_stylesheet_directory() . '/functions/meta-boxes.php' );
+	
+	// Load custom widgets
+	load_template( get_stylesheet_directory() . '/functions/widgets/alx-tabs.php' );
+	load_template( get_stylesheet_directory() . '/functions/widgets/alx-video.php' );
 	load_template( get_stylesheet_directory() . '/functions/widgets/alx-posts.php' );
+	
+	// Load custom shortcodes
+	load_template( get_stylesheet_directory() . '/functions/shortcodes.php' );
+
+	// Load dynamic styles
+	load_template( get_stylesheet_directory() . '/functions/dynamic-styles.php' );
+
+
 }
 
 add_action( 'after_setup_theme', 'alx_load' );	
---
+++ page-templates/slider-content.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: Slider Content
+*/
+?>
+<?php get_header(); ?>
+
+<section class="content">
+	
+	<?php get_template_part('inc/page-title'); ?>
+
+	<div class="pad group">
+		
+		<?php get_template_part('inc/featured'); ?>
+	
+		
+		<?php while ( have_posts() ): the_post(); ?>
+		
+			<article <?php post_class('group'); ?>>
+				
+				<?php get_template_part('inc/page-image'); ?>
+				
+				<div class="entry">
+					<?php the_content(); ?>
+					<div class="clear"></div>
+				</div><!--/.entry-->
+				
+			</article>
+			
+			<?php if ( ot_get_option('page-comments') != '' ) { comments_template('/comments.php',true); } ?>
+			
+		<?php endwhile; ?>
+		
+	</div><!--/.pad-->
+	
+</section><!--/.content-->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
